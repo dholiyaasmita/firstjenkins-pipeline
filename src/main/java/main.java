bdacfdbcbfe73cac6d9389fe1f43869c18aaba94
Asmi <maredia1990@gmail.com>
2020-11-19 10:00:30 -0500

    Added message for error in main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,6 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello World.......");
         System.out.println("Changes made by me.....");
-        System.out.println("Buid failure....");
+        System.out.println("Build failure....don't know the error");
     }
 }
